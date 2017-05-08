9ae54bab2ff80cfa4d5becb537b7f8dc49f00b13
mullak99 <mullak99@mullak99.co.uk>
2017-05-08 21:49:35 +0100

    Initial Release

---
+++ Checksums/Checksums.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class Checksums
+{
+    public string convertHashToString(byte[] hash)
+    {
+        return BitConverter.ToString(hash).Replace("-", "");
+    }
+
+    public bool compareHash(byte[] firstHash, byte[] secondHash)
+    {
+        if (convertHashToString(firstHash) == convertHashToString(secondHash)) return true;
+        else return false;
+    }
+
+    public byte[] getMD5(string inputFile)
+    {
+        using (var md5 = MD5.Create())
+        using (var stream = File.OpenRead(inputFile))
+            return md5.ComputeHash(stream);
+    }
+
+    public byte[] getSHA1(string inputFile)
+    {
+        using (var sha1 = SHA1.Create())
+        using (var stream = File.OpenRead(inputFile))
+            return sha1.ComputeHash(stream);
+    }
+
+    public byte[] getSHA256(string inputFile)
+    {
+        using (var sha256 = SHA256.Create())
+        using (var stream = File.OpenRead(inputFile))
+            return sha256.ComputeHash(stream);
+    }
+
+    public byte[] getSHA512(string inputFile)
+    {
+        using (var sha512 = SHA512.Create())
+        using (var stream = File.OpenRead(inputFile))
+            return sha512.ComputeHash(stream);
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Checksums
+A C# library used to grab, and compare, file checksums using various hashing algorithms.
+
+
+//TODO - Complete this section.
+
+# Download
+The Latest Releases can always be found at either:
+
+http://github.com/mullak99/Checksums/releases/latest
+
+http://builds.mullak99.co.uk/Checksums/latest
+
+# Usage
+
+- Add the library as a referance to your project
+- Create an instance of the library within your code
+
+
+//TODO - Complete this section.
+
+# Example Code
+
+Checksums CS = new Checksums();
+
+CS.getMD5(inputFile);
+
+
+//TODO - Complete this section.
+
+# Changelog
+
+|---| 1.0.0.0 |---|
+
+- Initial Release
